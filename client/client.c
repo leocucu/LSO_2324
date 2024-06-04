31c188e24c3659cc8797a52da200b06c56f9ef5e
Leo Cucurachi <cucurachileo01@gmail.com>
2024-06-04 19:33:34 +0200

    added client socket init

--- client/client.c
+++ client/client.c
@@ -1,1 +1,43 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <arpa/inet.h>
 
+#include <ncurses.h>
+
+#define PORT 12345
+#define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+
+int main(){
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    // Create socket
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    if(inet_pton(AF_INET, SERVER_IP, &servaddr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //  Login o Registrazione
+
+    //  Inizio del ciclo:
+    //  Lista stanze
+    //  Apertura chat
+
+    return 0;
+}
--- server/server.c
+++ server/server.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
 
+//  Struct client
+
+//  Array di client
+//  Mutex per i client
+
+int main(){
+    //  Inizializzazione DB
+
+    //  Creazione e Bind socket
+    
+    //  Inizio ciclo:
+    //  accettazione del client
+    //  creazione del thread per gestire il client
+
+    return 0;
+}
+
+//  Handler del client
